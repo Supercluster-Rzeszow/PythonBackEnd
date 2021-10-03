3ff20ba98cc0aa0f29f6fe32bb0d416794765e49
WiktorZmuda <30571899+WiktorZmuda@users.noreply.github.com>
2021-10-03 12:22:27 +0200

    Add files via upload

--- StandardAtmosphere.py
+++ StandardAtmosphere.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import pandas as pd
 
 e   =   2.71828182846
 def Height():
@@ -32,7 +33,7 @@ def Temperature(H):
         Tb      =   270.65
         Beta    =   -0.0028   
         Hb      =   51000
-    elif i > 71000 and i <= 84000:
+    elif H > 71000 and H <= 84000:
         Tb      =   214.65
         Beta    =   -0.002   
         Hb      =   71000
@@ -108,8 +109,8 @@ def SpeedOfSound(Th):
 def AscentLoop(height):
     GM      =   3.986004e+14
     ER      =   6.3781e+06
-    SimBase =   0.1
-    SimTime =   20
+    SimBase =   0.5
+    SimTime =   72000
     TimeVector = np.arange(0,SimTime, SimBase)
     T           =   [0] * len(TimeVector)
     p           =   [0] * len(TimeVector)
@@ -131,13 +132,17 @@ def AscentLoop(height):
     time_to_burst = 0
     neck_lift = 0
     launch_radius = 0
-    V_b[0] = 2          # initial baloon volume
+    V_b[0] = 1.76          # initial baloon volume 2 , 1 0.1
     t_0 = 0
     rho_He = 0.1786
     C_d = 0.3
-    BaloonMass = 1
-    PayloadMass = 0.3
+    BaloonMass = 0.8
+    PayloadMass = 0.25
     TotalMass = BaloonMass + PayloadMass
+    r_pop = 3.5
+    pop_time = 0
+    pop_height = 0
+    
 
     #burst_volume = (4.0/3.0) * math.pi * pow(balloon_burst_diameter / 2.0, 3)
 
@@ -149,12 +154,12 @@ def AscentLoop(height):
     rho[0]          = Density(T[0], p[0])
     k_constant      = (p[0]*V_b[0])/T[0]
     g[0]            = GM / (ER + h[0])**2
-    F_B[0]          = -1 * V_b[0] * (rho_He - rho[0]) * g[0]
     F_G[0]          = TotalMass * g[0]
     F_D[0]          = 0
     V_b[0]          = (k_constant * T[0])/p[0]
     A_b[0]          = math.pi * ((3*V_b[0])/(4*math.pi))**(2/3)
-    r[0]            = math.sqrt(A_b[0]/math.pi) 
+    r[0]            = math.sqrt(A_b[0]/math.pi)
+    F_B[0]          = -1 * V_b[0] * (rho_He - rho[0]) * g[0]
     a[0]            = 0
     v[0]            = 0
     calka_v         = 0
@@ -174,36 +179,32 @@ def AscentLoop(height):
         F_D[i]          = 0.5*C_d*A_b[i]*((v[i-1])**2)
 
         a[i]            = (F_B[i] - F_G[i] - F_D[i])/TotalMass
-        calka_v         = calka_v + (a[i] + a[i-1]) * 0.5 * SimBase
-        v[i]            = calka_v
+        #ascent_rate[i]  = 2*(F_B[i] - F_G[i])/(C_d * Pressure(height[i], Temperature(height[i])) * A_b[i])
+        #v[i]     = math.sqrt(2*(F_B[i] - F_G[i])/(C_d * rho[i] * A_b[i]))
+        v[i]            = math.sqrt((2*(F_G[i]))/(C_d * rho[i] * A_b[i]))
+        #calka_v         = calka_v + (a[i] + a[i-1]) * 0.5 * SimBase
+        #v[i]            = calka_v
         calka_h         = calka_h + (v[i] + v[i-1]) * 0.5 * SimBase
         h[i]            = calka_h
+
+        if r[i] >= r_pop:
+            pop_time    = i
+            pop_height  = h[i]
+            break
         
         #V_b[i]          = 
         #A_b[i]          = 
-        #ascent_rate[i]   = 2*(F_B[i] - F_G[i])/(C_d * Pressure(height[i], Temperature(height[i])) * A_b[i])
+        #
         #F_D[i]           = C_d * Pressure(height[i], Temperature(height[i])) * pow(ascent_rate,2) * A_b[i] / 2
 
-    
+    data = pd.DataFrame(np.transpose(np.array([TimeVector, h, v, a, T, p, rho, g, F_G, F_B, F_D, V_b, A_b, r])),
+                    columns=['Czas [s]', 'Wysokosc [m]', 'Predkosc pionowa [m/s]', 'Przyspieszenie[m/s^2]', 'Temperatura [K]', 'Cisnienie [Pa]', 'Gestosc [kg/m^3]', 'Przyspieszenie grawitacyjne [m/s^2]', 'Siła ciężkości [N]', 'Siła wyporu [N]', 'Siła oporu aerodynamicznego [N]', 'Objetosc balonu [m^3]', 'Powierzchnia czolowa balonu [m^2]', 'Promien balonu [m]'])
                                 
-    return r, A_b, V_b, h, T, p, g, F_G, F_B, F_D, a, v, h, TimeVector
+    return data, pop_time, pop_height
 #-----------------------------------------------
 
 #-----------------------------------------------
 height          =   Height()
 
-r, A_b, V_b, h, T, p, g, F_G, F_B, F_D, a, v, h, TimeVector = AscentLoop(height)
-print(r)
-print(A_b)
-print(V_b)
-print(h)
-print(T)
-print(p)
-print(g)
-print(F_G)
-print(F_B)
-print(F_D)
-print(a)
-print(v)
-print(h)
-print(TimeVector)
+data, pop_time, pop_height = AscentLoop(height) #change
+print(data)
